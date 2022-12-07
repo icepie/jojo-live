c9bf954f61b3a190159a5326fdd8b4faace3245e
Tea <icepie.dev@gmail.com>
2022-12-08 00:36:20 +0800

    fix ma ac

--- termux/client/ma.go
+++ termux/client/ma.go
@@ -5,20 +5,22 @@ import (
 	ma "jojo-live/midea-ac"
 )
 
-// var (
-// 	MideaAc ma.AirConditioningDevice
-// )
+var (
+	MideaAcDevice ma.Device
+)
 
-func GetMaAcIndoorTemperature() (temperature float64, err error) {
+func init() {
+	MideaAcDevice = ma.NewDevice("192.168.2.217", 162727724063545, "6444")
 
-	device := ma.NewDevice("192.168.2.217", 162727724063545, "6444")
-
-	isAuth := device.AuthenticateV3("0952ccb99ed54264bb5774e68b126acb24f7f61aa7c143b5a06af9601057b7ac", "6EBCEACB480D0F68395A05F2D9E96299379477CF986950E770103FADB29232E806619956B3BCA2C9660EAA86C5DB7C0891587F7613CDD0A12B14A86B5E252BF2")
+	isAuth := MideaAcDevice.AuthenticateV3("0952ccb99ed54264bb5774e68b126acb24f7f61aa7c143b5a06af9601057b7ac", "6EBCEACB480D0F68395A05F2D9E96299379477CF986950E770103FADB29232E806619956B3BCA2C9660EAA86C5DB7C0891587F7613CDD0A12B14A86B5E252BF2")
 	if !isAuth {
 		panic("Authentication failed")
 	}
+}
 
-	MideaAc := ma.NewAirConditioningDevice(device)
+func GetMaAcIndoorTemperature() (temperature float64, err error) {
+
+	MideaAc := ma.NewAirConditioningDevice(MideaAcDevice)
 
 	MideaAc.Refresh()
 
