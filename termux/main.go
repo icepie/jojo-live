b8cd574ef92b966b4b7c8906c5e0867a4ddeebfe
Tea <icepie.dev@gmail.com>
2022-12-08 00:13:53 +0800

    mi light power

--- termux/main.go
+++ termux/main.go
@@ -5,7 +5,6 @@ import (
 	"os/exec"
 
 	"jojo-live/client"
-	ma "jojo-live/midea-ac"
 
 	tm "github.com/eternal-flame-AD/go-termux"
 )
@@ -52,14 +51,5 @@ func main() {
 		log.Println(stat.Health, stat.Percentage, stat.Status, stat.Temperature)
 	}
 
-	device := ma.NewDevice("192.168.2.217", 162727724063545, "6444")
-
-	isAuth := device.AuthenticateV3("0952ccb99ed54264bb5774e68b126acb24f7f61aa7c143b5a06af9601057b7ac", "6EBCEACB480D0F68395A05F2D9E96299379477CF986950E770103FADB29232E806619956B3BCA2C9660EAA86C5DB7C0891587F7613CDD0A12B14A86B5E252BF2")
-	if !isAuth {
-		panic("Authentication failed")
-	}
-
-	ac := ma.NewAirConditioningDevice(device)
-
-	log.Println(ac.IndoorTemperature(), ac.Outdoortemperature(), ac.PowerState(), ac.SwingMode(), ac.FanSpeed(), ac.TurboMode())
+	log.Println(client.MideaAc.IndoorTemperature(), client.MideaAc.Outdoortemperature(), client.MideaAc.PowerState(), client.MideaAc.SwingMode(), client.MideaAc.FanSpeed(), client.MideaAc.TurboMode())
 }
