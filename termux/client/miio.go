bbd235bb4b66a2b36ca01f5e18dee4f4f6e55e04
Tea <icepie.dev@gmail.com>
2022-12-08 01:11:36 +0800

    fix con

--- termux/client/miio.go
+++ termux/client/miio.go
@@ -7,20 +7,14 @@ import (
 	"github.com/icepie/miio.go"
 )
 
-var (
-	MiLight *miio.Client
-)
-
-func init() {
-	MiLight = miio.New("192.168.2.214").SetToken("1b89c51e6d9d95a36300238b77170a98").SetDid("")
-}
-
 type MiLightStatus struct {
 	Id     int           `json:"id"`
 	Result []interface{} `json:"result"`
 }
 
 func GetMiLightStatus() (status MiLightStatus, err error) {
+
+	MiLight := miio.New("192.168.2.214").SetToken("1b89c51e6d9d95a36300238b77170a98").SetDid("")
 
 	// for get siid and piid, see https://home.miot-spec.com/
 	// get properties
@@ -44,6 +38,9 @@ func GetMiLightStatus() (status MiLightStatus, err error) {
 }
 
 func SetMiLightPower(power bool) (err error) {
+
+	MiLight := miio.New("192.168.2.214").SetToken("1b89c51e6d9d95a36300238b77170a98").SetDid("")
+
 	if power {
 		// set properties
 		_, err = MiLight.Send("set_power", []interface{}{"on", "smooth", 500})
